930a700e5b649b4882489c6dd5dda8a2aa334095
alivkas <mr1504dj@mail.ru>
2023-11-09 15:46:12 +0500

    add permissions

--- study_project/urls.py
+++ study_project/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.registration),
     path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('myclass/', views.class_autorize, name='class'),
+    path('myclass/', views.class_create, name='class'),
     path('edit_account/', views.edit_profile, name='edit_profile')
 ]
--- study_project/views.py
+++ study_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User, Group
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .forms import SignUpForm, ClassForm, PersonForm
 from .models import Class, Person
 
@@ -57,16 +57,22 @@ def edit_profile(request):
 
 
 @login_required(login_url='login')
-def class_autorize(request):
+def class_create(request):
     if request.method == 'POST':
         form = ClassForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             bio = form.cleaned_data['bio']
-            school_class = Class.objects.create(name=name, bio=bio)
-            school_class.save()
-            Person.objects.update(school_class=school_class)
-            return redirect('home')
+            if request.user.has_perms(['study_project.add_class', 'study_project.change_class']):
+                school_class = Class.objects.create(name=name, bio=bio)
+                school_class.save()
+                Person.objects.update(school_class=school_class)
+                return redirect('home')
+            else:
+                school_class = Class.objects.get(name=name)
+                school_class.save()
+                Person.objects.update(school_class=school_class)
+                return redirect('home')
     else:
         form = ClassForm()
     return render(request, 'school_class.html', {'form': form})
