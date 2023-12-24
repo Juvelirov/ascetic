3ed0762f58b14655404b48dffc7bb8a6bd2db28c
alivkas <mr1504dj@mail.ru>
2023-12-24 16:04:56 +0500

    fix bug

--- projectX/settings.py
+++ projectX/settings.py
@@ -95,9 +95,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
+USE_TZ = True
 
 USE_I18N = True
 
